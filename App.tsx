18fe68ed63770302c5d968ea4b1be27233fca1bf
Igor <io3323@list.ru>
2022-11-21 15:46:34 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
